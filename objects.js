1c6f3a28ed74f33e5e758c0594a0465471b84984
brainy-language-7409 <castlesofcode@gmail.com>
2019-02-08 06:43:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,4 +4,7 @@ var meals = new Object ({breakfast: 'oatmeal'})
 // two ways of creating Object
 //breakfast is the key and "oatmeal" is the value
 
-
+var playlist = {
+  slayer:'raining blood',
+  slayer: 'dancing in the moonlight'
+}
